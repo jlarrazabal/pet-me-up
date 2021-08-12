33c71cda12dbad7aee5aff23d4308bc473074fdb
jlarrazabal <69408299+jlarrazabal@users.noreply.github.com>
2021-08-11 21:41:57 -0400

    Updating connection file and user model
    Updating connection file and user model.

--- server/models/User.js
+++ server/models/User.js
@@ -25,7 +25,7 @@ const userSchema = new Schema({
     required: true,
     minlength: 5
   },
-  orders: [Order.schema]
+  orders: [Pet.schema]
 });
 
 // set up pre-save middleware to create password
