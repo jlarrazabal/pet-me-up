765ee8ee4da57f04214bba26cdfe2256bf2526b8
mayraorlando <44445250+mayraorlando@users.noreply.github.com>
2021-08-19 20:13:16 -0400

    added stripe
    added stripe

--- client/src/components/AppointmentSummary.js
+++ client/src/components/AppointmentSummary.js
@@ -5,6 +5,7 @@ import { QUERY_GETAPPOINTMENTBYID,QUERY_GET_SERVICES  } from '../utils/queries';
 import { DELETE_APPOINTMENT } from '../utils/mutations';
 import './AppointmentSummary.css';
 
+
 export default function AppointmentSummary(){
 const {appointmentID} = useParams();
 // const [date, setDate] = useState('');
@@ -40,7 +41,9 @@ const [paymentID, setPaymentID] = useState("");
            <h1 className="appointmentHeader">Your appointment has been successfully created </h1>
            <div classname="summary "><h2 className='summary-form-input'>Appointment Summary</h2></div>
             <div className="date"><h3>Date:{data?.date}</h3></div>
+            <div {service.map(service =>)}>
             <div className="time"><h3>Time:{data?.time}</h3></div>
+            </div>
             <div classname="summary "><h3 className='summary-form-input'>List of Services</h3></div>
             <ul>{services.map(service => {return (<li>Service name:{service.name}, Service price:{service.name}</li>)})}</ul>
             <div className="totalPrice"><h3>Total Price: ${services.reduce((total, item) => {return total + item.price},0)}</h3></div>
