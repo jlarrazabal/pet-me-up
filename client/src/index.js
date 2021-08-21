08858cd6e8cc27700150cc1a20c6e7cfe5f4f0a6
psurer <surereker@icloud.com>
2021-08-21 12:55:51 -0400

    adding aws config

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './components/Header.css'
+import Amplify from 'aws-amplify';
+import config from './config';
 
 Amplify.configure({
   Auth: {
